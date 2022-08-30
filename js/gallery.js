a7a32dbaee48ceb44c68c4a6d1fcd91acdf1c597
Anna Markovskaya <109307073+AnnaMark90@users.noreply.github.com>
2022-08-30 14:03:27 +0300

    Update gallery.js

--- js/gallery.js
+++ js/gallery.js
@@ -1,5 +1,42 @@
-const gallery = (id) => {
-    
+const gallery = (selector) => {
+    const gallery = document.getElementById(selector);
+
+    let elem = gallery.getElementsByClassName('gallery__thumbs')[0].children;
+
+    [...elem].forEach((elem) => {
+        elem.addEventListener('click', (e) => show(e));
+    });
+
+    function show(e) {
+        e.preventDefault();
+
+        let preview = document.querySelector('.gallery__preview');
+
+        preview.style.visibility = "visible";
+        // preview.classList.remove('close');
+        let src = e.target.parentElement.href;
+        let img = document.createElement('img');
+        img.setAttribute('href', '#');
+        img.setAttribute('src', src);
+
+        preview.append(img);
+
+        // let close_pop = preview.nextElementSibling;
+        // close_pop.addEventListener('click', hide);
+
+        // function hide() {
+        //     preview.style.visibility = "hidden";
+        // };
+
+        let close_pop_up = (e) => {
+            preview.classList.add('close')
+        };
+
+        let closeBtn = document.querySelector('gallery__preview_close');
+        closeBtn.addEventListener('click', close_pop_up);
+    };
+
+
 };
 
 gallery('gallery'); // id
